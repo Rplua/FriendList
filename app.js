d5fbb904dfb807e2a23d14a03077aaa8a91a265a
Randy <rplua77@gmail.com>
2025-03-09 20:33:40 +0100

    new component

--- app.js
+++ app.js
@@ -5,7 +5,26 @@ const app = Vue.createApp({
                 {id: 'manuel', name: 'Manuel Perez', phone: '11111', email: 'eee@ww.com'},
                 {id: 'Holi', name: 'Holi Perez', phone: '12222', email: 'eee@ww.com'}
             ],
+        };
+    },
+   
+});
+
+app.component('friend-contact', {
+    template:`
+    <li>
+          <h2>{{friend.name}}</h2>
+          <button @click="toggleDetails"> {{detailsAreVisible ? 'Hide ' : 'Show '}}Details</button>
+          <ul v-if="detailsAreVisible">
+            <li><strong>Phone:</strong>{{friend.phone}}</li>
+            <li><strong>Email:</strong> {{friend.email}}</li>
+          </ul>
+        </li>
+    `,
+    data(){
+        return {
             detailsAreVisible: false,
+            friend: {id: 'manuel', name: 'Manuel Perez', phone: '11111', email: 'eee@ww.com'}, 
         };
     },
     methods:{
@@ -14,6 +33,4 @@ const app = Vue.createApp({
         }
     }
 });
-
-
 app.mount('#app')
